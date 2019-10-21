38ab5fa1bdee7fc0196782adde30e5e77c00073f
Dominic Watson <intellix@gmail.com>
2019-10-21 10:59:20 +0200

    Add ng-lazyload-image/universal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { isPlatformServer } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { LazyLoadImageModule, intersectionObserverPreset } from 'ng-lazyload-image';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+export function isBot(navigator, platformId) {
+  return isPlatformServer(platformId) ? true : intersectionObserverPreset.isBot(navigator, platformId);
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    LazyLoadImageModule.forRoot({
+      preset: intersectionObserverPreset,
+      isBot,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
